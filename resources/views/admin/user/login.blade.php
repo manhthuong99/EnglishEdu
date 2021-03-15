de5b26bc287acbdf5a88f9f208ce7f7a80de27ae
manhthuong99 <mthuong03@gmail.com>
2021-03-16 00:02:33 +0700

    fixed

--- resources/views/admin/user/login.blade.php
+++ resources/views/admin/user/login.blade.php
@@ -13,10 +13,10 @@
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
     <!-- Icons -->
-    <link rel="stylesheet" href="{{ secure_asset('assets/vendor/nucleo/css/nucleo.css') }}" type="text/css">
-    <link rel="stylesheet" href="{{ secure_asset('assets/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/nucleo/css/nucleo.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}" type="text/css">
     <!-- Argon CSS -->
-    <link rel="stylesheet" href="{{ secure_asset('assets/css/argon.css?v=1.1.0') }}" type="text/css">
+    <link rel="stylesheet" href="{{ asset('assets/css/argon.css?v=1.1.0') }}" type="text/css">
 </head>
 
 <body class="bg-default">
